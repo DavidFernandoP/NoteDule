5b8f8a2c05e0058f5a0ae24dba116e9284853e4b
DavidFernandoP <davidparra0101@gmail.com>
2024-05-09 17:12:58 -0500

    métodos apuntes tareas

--- lib/modelos/apunte.dart
+++ lib/modelos/apunte.dart
@@ -1,9 +1,33 @@
 // lib/models/apunte.dart
 class Apunte {
+  final int? id;
   final String titulo;
   final String contenido;
   final DateTime fecha;
-  bool isSelected; 
+  final bool isSelected; 
 
-  Apunte({required this.titulo, required this.contenido, required this.fecha, this.isSelected = false});
+  const Apunte({
+    required this.titulo, 
+    required this.contenido, 
+    required this.fecha, 
+    this.isSelected = false,
+    this.id
+  });
+
+  factory Apunte.fromJson(Map<String,dynamic> json) => Apunte(
+    id: json['id'],
+    titulo: json['titulo'],
+    contenido: json['contenido'],
+    fecha: json['fecha'],
+    isSelected: json['isSelected'],
+  );
+
+  Map<String,dynamic> toJson() => {
+    'id': id,
+    'titulo': titulo,
+    'contenido': contenido,
+    'fecha': fecha,
+    'isSelected': isSelected,
+  };
 }
+
--- lib/servicios/database_helper.dart
+++ lib/servicios/database_helper.dart
@@ -1,4 +1,5 @@
 import 'package:notedule/modelos/materia_modelo.dart';
+import 'package:notedule/modelos/apunte.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:flutter/material.dart';
@@ -8,12 +9,23 @@ class DatabaseHelper {
   static const String _dbName = "baseDatos.db";
 
   static Future<Database> _getDB() async {
-    return openDatabase(join(await getDatabasesPath(), _dbName),
-        onCreate: (db, version) async => await db.execute(
-            "CREATE TABLE Materia(id INTEGER PRIMARY KEY, nombre TEXT NOT NULL, color TEXT NOT NULL, dia TEXT NOT NULL, inicio TEXT NOT NULL, fin TEXT NOT NULL);"),
-        version: _version);
+    return openDatabase(
+      join(await getDatabasesPath(), _dbName),
+        onCreate: (db, version) async { 
+          await db.execute(
+              "CREATE TABLE Materia(id INTEGER PRIMARY KEY, nombre TEXT NOT NULL, color TEXT NOT NULL, dia TEXT NOT NULL, inicio TEXT NOT NULL, fin TEXT NOT NULL);"
+          );
+          /*await db.execute(
+              "CREATE TABLE Apuntes(id INTEGER PRIMARY KEY, materia TEXT NOT NULL, titulo TEXT NOT NULL, fecha DATE NOT NULL, contenido TEXT NOT NULL, isSelected TEXT NOT NULL);",
+          );
+          await db.execute(
+              "CREATE TABLE Tareas(id INTEGER PRIMARY KEY, materia TEXT NOT NULL, titulo TEXT NOT NULL, fecha DATE NOT NULL, contenido TEXT NOT NULL, check TEXT NOT NULL);",
+          );*/
+        },
+        version: _version,
+    );
   }
-
+//---------------------Metodos para materias------------------------------------
   static Future<int> addNote(Materia note) async {
     final db = await _getDB();
     return await db.insert("Materia", note.toJson(),
@@ -83,4 +95,70 @@ class DatabaseHelper {
 
     return defaultMaterias;
   }
+  /*
+  //---------------------Metodos para apuntes-------------------------
+  static Future<int> addApunte(Apunte apunte) async {
+    final db = await _getDB();
+    return await db.insert("Apuntes", apunte.toJson(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  static Future<int> updateApunte(Apunte apunte) async {
+    final db = await _getDB();
+    return await db.update("Apuntes", apunte.toJson(),
+        where: 'titulo = ?',
+        whereArgs: [apunte.titulo],
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  static Future<int> deleteApunte(Apunte apunte) async {
+    final db = await _getDB();
+    return await db.delete(
+      "Apuntes",
+      where: 'titulo = ?',
+      whereArgs: [apunte.titulo],
+    );
+  }
+
+  static Future<List<Map<String, dynamic>>> getApuntes(String materia) async {
+    final db = await _getDB();
+    return await db.query(
+      'Apuntes',
+      where: 'materia = ?',
+      whereArgs: [materia],
+    );
+  }
+  
+  //----------------------Metodos para tareas--------------------------
+  static Future<int> addTarea(Tarea tarea) async {
+    final db = await _getDB();
+    return await db.insert("Tareas", tarea.toJson(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  static Future<int> updateTarea(Tarea tarea) async {
+    final db = await _getDB();
+    return await db.update("Tareas", tarea.toJson(),
+        where: 'titulo = ?',
+        whereArgs: [tarea.titulo],
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  static Future<int> deleteTarea(Tarea tarea) async {
+    final db = await _getDB();
+    return await db.delete(
+      "Tareas",
+      where: 'titulo = ?',
+      whereArgs: [tarea.titulo],
+    );
+  }
+
+  static Future<List<Map<String, dynamic>>> getTarea(String materia) async {
+    final db = await _getDB();
+    return await db.query(
+      'Tareas',
+      where: 'materia = ?',
+      whereArgs: [materia],
+    );
+  }*/
 }
